a58fb438af995f498b981e1529298bd01cf5c2f6
febrahimi <ariaei.farhad@gmail.com>
2026-02-12 22:14:38 +0330

    initial commit

---
+++ src/test/java/com/farhad/realestateservice/RealEstateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farhad.realestateservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
